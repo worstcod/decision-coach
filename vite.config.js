5552fb3ee7d74e2ac97f7d51db4e6f0122e11d10
wrostcod <praveenjoshi58.pj@gmail.com>
2026-05-19 20:24:58 +0530

    Fix vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
-  base: './',
+  plugins: [react()],
+  base: '/decision-coach/',
 })
